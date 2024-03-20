eb6539c28403f8d3660742972cfe5b4f76735833
liamor2 <liam@gattegno.net>
2024-03-20 16:48:39 +0100

    Secured mongodb connection in .env file

--- ecocovoit/app.js
+++ ecocovoit/app.js
@@ -1,26 +1,20 @@
 const express = require('express');
 const app = express();
 const port = 3001;
-const { MongoClient } = require('mongodb');
-const uri = "mongodb://localhost:27017/cities_app";
-const client = new MongoClient(uri, { useNewUrlParser: true });
-const db = client.db("cities_app");
 const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+dotenv.config();
 
 
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: true }));
 
-
-
-mongoose.connect('mongodb://localhost:27017/cities_app').then(() => console.log('Connecté avec succès à MongoDB')).catch((err) => console.error('Erreur lors de la connexion à MongoDB :', err));
-
-const City = mongoose.model('City', { name: String, uuid: String });
-
-// Démarrage du serveur
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log('Connected to MongoDB'))
+  .catch(err => console.error('Could not connect to MongoDB', err));
 
 app.listen(port, () => {
-  console.log(`Exemple d'application écoutant sur http://localhost:${port}`);
+  console.log(`Serveur started on http://localhost:${port}`);
 });
 
 app.get('/', (req, res) => {
