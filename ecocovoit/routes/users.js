0a8e7579ec86fe89c92e0e5a6010b9d76ddc676d
SVergnol <samuel.vergnol@lucem-conseil.com>
2024-03-20 18:07:23 +0100

    functional getAllUsers

--- ecocovoit/routes/users.js
+++ ecocovoit/routes/users.js
@@ -1,13 +1,14 @@
 var express = require('express');
 var router = express.Router();
-var userSchema = require('../schemas').User;
+var userSchema = require('../schemas').User
 
-router.get('/api/users', function(req, res, next) {
-  userSchema.find({}).then(users => {
-    res.status(200).send(users);
-  }).catch(err => {
-    res.status(500).send('Error');
-  });
+
+router.get('/api/users',(req,res) => {
+  userSchema.find({}).then(
+    data => res.send(data)
+  ).catch(
+    err => { throw err; }
+  );
 });
 
 router.get('/api/users/:id', function(req, res, next) {
